18f3a59bd180a3778d780bbf737cf3a9f724fe3f
Javier Palacios <javiplx@gmail.com>
2014-05-21 21:59:17 +0200

    Update npm_spec.rb

--- spec/unit/puppet/provider/pakages/npm_spec.rb
+++ spec/unit/puppet/provider/pakages/npm_spec.rb
@@ -38,7 +38,7 @@ describe Puppet::Type.type(:package).provider(:npm) do
   end
 
   describe "when installing npm packages" do
-    before { @resource[:install_options] = {'registry'=>'http://custom.registry/npm'} }
+    before { @resource[:install_options] = [ {'registry'=>'http://custom.registry/npm'} ] }
     describe "and install options are specified" do
       it "should use the given registry" do
         @provider.expects(:npm).with('install', '--global', '--registry', 'http://custom.registry/npm', 'express')
