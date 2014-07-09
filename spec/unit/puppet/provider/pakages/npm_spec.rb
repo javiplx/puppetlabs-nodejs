4584dc09a12cc3e35cd8142ef3571156c4a1838e
Javier Palacios <javiplx@gmail.com>
2014-07-09 20:22:50 +0200

    Handle install_options as array

--- spec/unit/puppet/provider/pakages/npm_spec.rb
+++ spec/unit/puppet/provider/pakages/npm_spec.rb
@@ -29,10 +29,10 @@ describe Puppet::Type.type(:package).provider(:npm) do
     end
 
     describe "and install options are specified" do
-      before { @resource[:install_options] = {'registry'=>'http://custom.registry/npm'} }
+      before { @resource[:install_options] = [{'registry'=>'http://custom.registry/npm'} }]
 
       it "should use the given registry" do
-        @provider.expects(:npm).with('install', '--global', '--registry', 'http://custom.registry/npm', 'express')
+        @provider.expects(:npm).with('install', '--global', ['--registry', 'http://custom.registry/npm'], 'express')
         @provider.install
       end
     end
